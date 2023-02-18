a4a15be8c36565cbe35cdaa338ee280a7a9461c0
Aleksey Rublev <AAlekseyRublev@gmail.com>
2023-02-18 17:37:47 +0530

    add file SQL-code

---
+++ DataCleaning_SQL/Create_table_NasvillHouse.sql
@@ -0,0 +1,24 @@
+CREATE TABLE NashvilleHouse
+(
+    UniqueID VARCHAR(120),
+    ParcelID VARCHAR(120),
+    LandUse VARCHAR(120),
+    PropertyAddress VARCHAR(120),
+    SaleDate VARCHAR(120),
+    SalePrice VARCHAR(120),
+    LegalReference VARCHAR(120),
+    SoldAsVacant VARCHAR(120),
+    OwnerName VARCHAR(120),
+    OwnerAddress VARCHAR(120),
+    Acreage VARCHAR(120),
+    TaxDistrict VARCHAR(120),
+    LandValue VARCHAR(120),
+    BuildingValue VARCHAR(120),
+    TotalValue VARCHAR(120),
+    YearBuilt VARCHAR(120),
+    Bedrooms VARCHAR(120),
+    FullBath VARCHAR(120),
+    HalfBath VARCHAR(120)
+);
+
+SELECT * FROM NashvilleHouse;
